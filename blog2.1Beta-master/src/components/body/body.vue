c48f821bcd785e88103eea5c9052c5f2123a9ca8
a <131>
2019-01-16 17:36:48 +0800

    001

---
+++ blog2.1Beta-master/src/components/body/body.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="body_container">
+        <!-- <div class="test" @click="addWindow"></div> -->
+        <Desktop></Desktop>
+        <notifyPop></notifyPop>
+        <Sidebar></Sidebar>
+        <component v-for="i in this.$store.state.windowItem" :is="i.component" :label="i.label" :id="i.id"></component>
+    </div>
+</template>
+
+<script>
+import Desktop from './desktop'
+import Sidebar from './sidebar'
+import window from './window'
+import notifyPop from '../Public/notifyPop'
+export default {
+    components: {
+        Desktop,
+        Sidebar,
+        window,
+        notifyPop
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+.body_container {
+    display: flex;
+    align-items: center;
+    color: #B0B6B6;
+    width: 100vw;
+    height: calc(100vh - 33px);
+}
+/* .test {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    background-color: #666;
+} */
+</style>
---
+++ blog2.1Beta-master/src/components/websocket/websocket_sysMonitor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="websocket_sysMonitor_container">
+        monitor
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        createWebSocket() {
+            // this.$store.dispatch('createSysMonitorWebSocket')
+        }
+    },
+    created() {
+        this.createWebSocket()
+    }
+}
+</script>
+
+<style scoped>
+.websocket_sysMonitor_container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 4px;
+    color: #B0B6B6;
+    background-color: #666;
+    box-shadow: 0 0 3px #B0B6B6 inset; 
+    margin-right: 6px;
+    padding: 0 6px;
+    cursor: default;
+}
+</style>
